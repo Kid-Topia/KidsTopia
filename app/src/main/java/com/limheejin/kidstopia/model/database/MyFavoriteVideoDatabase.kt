77a1b435cac45e25349f7c2e43ee58a5207635af
bonggyulim <qhdrbdhkdwks@naver.com>
2024-05-20 13:54:56 +0900

    [#33] Feat: migrations 후 db버전업

--- app/src/main/java/com/limheejin/kidstopia/model/database/MyFavoriteVideoDatabase.kt
+++ app/src/main/java/com/limheejin/kidstopia/model/database/MyFavoriteVideoDatabase.kt
@@ -1,24 +1,30 @@
 package com.limheejin.kidstopia.model.database
 
-import android.app.Application
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import com.google.gson.Gson
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [MyFavoriteVideoEntity::class], exportSchema = false, version = 1)
+@Database(entities = [MyFavoriteVideoEntity::class], exportSchema = false, version = 2)
 abstract class MyFavoriteVideoDatabase: RoomDatabase() {
     abstract fun getDao(): MyFavoriteVideoDAO
 
     companion object {
         private var INSTANCE: MyFavoriteVideoDatabase? = null
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE video_table ADD COLUMN 'isLikedDate' TEXT")
+            }
+        }
 
         fun getDatabase(context: Context) : MyFavoriteVideoDatabase {
+
             if (INSTANCE == null) {
                 INSTANCE = Room.databaseBuilder(
                     context.applicationContext, MyFavoriteVideoDatabase::class.java, "video_database")
+                    .addMigrations(MIGRATION_1_2)
                     .build()
             }
             return INSTANCE as MyFavoriteVideoDatabase
