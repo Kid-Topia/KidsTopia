eb540a28493389091817256ed435db0cfe927188
xeejin <heezeenee@gmail.com>
2024-05-22 20:09:45 +0900

    [#39] Fix: ScrollView, 네비게이션바 디테일 겹침 오류 해결

--- app/src/main/java/com/kidstopia/kidstopia/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/kidstopia/kidstopia/presentation/activity/MainActivity.kt
@@ -31,11 +31,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
--- app/src/main/java/com/kidstopia/kidstopia/presentation/network/NetworkClient.kt
+++ app/src/main/java/com/kidstopia/kidstopia/presentation/network/NetworkClient.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object NetworkClient {
     private const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
-    const val AUTH_KEY = "AIzaSyCR08D5FBu7tW5yE8kqFFZLWiaFKRv-V8g"
+    const val AUTH_KEY = "AIzaSyDDdGC-idaPyf4f_k667l_gISrzhFj-qvk"
 
     private fun createOkHttpClient(): OkHttpClient {
         val interceptor = HttpLoggingInterceptor()
